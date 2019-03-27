d8feffc636e9f20e9c728f5f975f8e473461613e
pdd <532963277@qq.com>
2019-03-27 16:41:30 +0800

    开始后台开发1

---
+++ src/main/java/top/maybe123/service/AuthorServicce.java
@@ -0,0 +1,24 @@
+package top.maybe123.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import top.maybe123.dao.BysjAuthorMapper;
+import top.maybe123.pojo.BysjAuthor;
+
+import java.util.List;
+
+/*
+** 作者信息查询
+*  @author pdd
+ */
+@Service
+public class AuthorServicce  {
+@Autowired
+ BysjAuthorMapper bysjAuthorMapper;
+public List<BysjAuthor> getAuthorByname(String name){
+
+
+    BysjAuthor bysjAuthor=new BysjAuthor();
+    return bysjAuthorMapper.queryBysjAuthor(bysjAuthor);
+}
+}
