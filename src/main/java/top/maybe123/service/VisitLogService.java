cae87428fd67f31d0317ab986738773e3b901ac7
pandd <532963277@qq.com>
2020-09-30 13:59:56 +0800

    提交错误修改。

--- src/main/java/top/maybe123/service/VisitLogService.java
+++ src/main/java/top/maybe123/service/VisitLogService.java
@@ -2,8 +2,8 @@ package top.maybe123.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import top.maybe123.dao.JlVisitlogMapper;
-import top.maybe123.pojo.JlVisitlog;
+//import top.maybe123.dao.JlVisitlogMapper;
+//import top.maybe123.pojo.JlVisitlog;
 
 import java.util.Iterator;
 import java.util.List;
@@ -13,12 +13,12 @@ import java.util.List;
  */
 @Service
 public class VisitLogService {
-    @Autowired
-    JlVisitlogMapper jlVisitlogMapper;
-    public void addVisitRecord(List<JlVisitlog> list){
-        for(Iterator<JlVisitlog> it=list.iterator();it.hasNext();){
-            JlVisitlog jlVisitlog=it.next();
-            jlVisitlogMapper.insertJlVisitlog(jlVisitlog);
-        }
-    }
+//    @Autowired
+//    JlVisitlogMapper jlVisitlogMapper;
+//    public void addVisitRecord(List<JlVisitlog> list){
+//        for(Iterator<JlVisitlog> it=list.iterator();it.hasNext();){
+//            JlVisitlog jlVisitlog=it.next();
+//            jlVisitlogMapper.insertJlVisitlog(jlVisitlog);
+//        }
+//    }
 }
