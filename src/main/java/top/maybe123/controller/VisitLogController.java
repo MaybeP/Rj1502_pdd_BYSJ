c4dec9d7a55b131bb9170517bc11f2380f934f6c
pandd <532963277@qq.com>
2020-09-30 13:57:13 +0800

    提交错误修改。

--- src/main/java/top/maybe123/controller/VisitLogController.java
+++ src/main/java/top/maybe123/controller/VisitLogController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import top.maybe123.pojo.JlVisitlog;
+//import top.maybe123.pojo.JlVisitlog;
 import top.maybe123.service.VisitLogService;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +17,9 @@ public class VisitLogController {
     VisitLogService visitLogService;
     @RequestMapping(value = "addJLLogInfo.action")
     public @ResponseBody String addJLLogInfo(@Param("loginfo")String logInfo){
-        Gson gson=new Gson();
-        List<JlVisitlog> jlVisitlogs=gson.fromJson(logInfo, new TypeToken<ArrayList<JlVisitlog>>(){}.getType());
-        visitLogService.addVisitRecord(jlVisitlogs);
+//        Gson gson=new Gson();
+//        List<JlVisitlog> jlVisitlogs=gson.fromJson(logInfo, new TypeToken<ArrayList<JlVisitlog>>(){}.getType());
+//        visitLogService.addVisitRecord(jlVisitlogs);
         return "success!";
     }
 }
